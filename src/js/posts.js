0654796bf6a9f538892ce36ccc2fd0a8e03d6a72
adrianhelllo <hetessyadrian@gmail.com>
2026-03-12 15:25:09 +0100

    add updates mades in class

---
+++ src/js/posts.js
@@ -0,0 +1,34 @@
+export const profilok = [
+    {
+        nev: "",
+        leiras: "",
+        profilkep: ""
+    }
+]
+
+export const postok = [
+    {
+        nev: "hello"
+    },
+    {
+        nev: "hello2"
+    },
+    {
+        nev: "hello3"
+    },
+    {
+        nev: "hello4"
+    },
+    {
+        nev: "hello5"
+    },
+    {
+        nev: "hello6"
+    },
+    {
+        nev: "hello7"
+    },
+    {
+        nev: "hello8"
+    },
+]
