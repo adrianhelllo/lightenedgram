8065b580f0f7fa6e330f9334ece22e22fa9862e2
adrianhelllo <hetessyadrian@gmail.com>
2026-03-15 15:12:00 +0100

    link sources to data.js

--- src/js/data.js
+++ src/js/data.js
@@ -19,34 +19,60 @@ export const profilok = [
         becenev: "Jean-Jacques Rousseau",
         nev: "jeanjacq_rousseau",
         leiras: 
-        `kaptiany ird meg mar uristen`,
+        ` - `,
         profilkep: "../assets/site/img/"
     },
 ]
 
 export const postok = [
+    // tipus ertek: "kep" / "video"
     {
-        nev: "hello"
+        feltolto: "xx_napoleon_a_felvilagosult_xx",
+        sorszam: 1,
+        tipus: "kep",
+        leiras:
+        `Erőfeszítéseimnek megérett gyümölcse. Katonai zsenialitásomon végre Én lehetek e forradalmi ország császára. Szolgálni fogom a népem, irányításom alatt Franciaország világhatalommá fog válni. Aspirációimmal meghódítom majd Európa egészét, és hódolni fognak nekem! Csak Waterloo-nál ne kelljen, hogy csatázzak. Elég ijesztő hely.
+        Hálásan köszönöm tehetséges festészemnek a gyönyörű képet! Büszkén osztom meg szépségem a világgal.`
     },
     {
-        nev: "hello2"
+        feltolto: "xx_napoleon_a_felvilagosult_xx",
+        sorszam: 2,
+        tipus: "kep",
+        leiras:
+        `További sikerek háborúimmal! A francia terjeszkedés megállíthatatlan. Ismételten köszönöm remek festőmnek a festményt!`
     },
     {
-        nev: "hello3"
+        feltolto: "xx_napoleon_a_felvilagosult_xx",
+        sorszam: 3,
+        tipus: "kep",
+        leiras:
+        `A felvilágosult ember felvilágosult elvek szerint kell hogy éljen. A Napóleoni Kódexben meghatározottak társadalmunkat új magaslatokra fogják juttatni. Minden polgár egyenlő a törvény szemében! A tulajdon joga természetesen eredendő! Teljes vallásszabadság! A nők alábbvalók, mint a férfiak!
+        ℹ️ Szeretne többet megtudni jogi eszméimről? Vegyen másolatot a Napóleoni Kódexről a helyi könyvárúsnál - akár 7 frankért!`
     },
     {
-        nev: "hello4"
+        feltolto: "xx_napoleon_a_felvilagosult_xx",
+        sorszam: 4,
+        tipus: "kep",
+        leiras:
+        `Legszörnyebb rémálmom mégis valóra vált! A hetedik koalíció ellenem szerveződött. Köszönöm a képet fotográfusomnak!
+        ℹ️ Szeretne többet megtudni jogi eszméimről? Vegyen másolatot a Napóleoni Kódexről a helyi könyvárúsnál - akár 7 frankért!`
     },
     {
-        nev: "hello5"
+        feltolto: "xx_napoleon_a_felvilagosult_xx",
+        sorszam: 5,
+        tipus: "kep",
+        leiras:
+        `Számüzettek erre a Szent Ilona szigetére azok a nyomorult ebek! Ó, bár ne szerettem volna meghódítani az egész világot. Isten majd csak megsegít. Addig is, béke legyen veletek, majd később még írok!
+        ℹ️ Szeretne többet megtudni jogi eszméimről? Vegyen másolatot a Napóleoni Kódexről a helyi könyvárúsnál - akár 7 frankért!`
     },
+
     {
-        nev: "hello6"
+        feltolto: "isaacnewton9812",
+        sorszam: 1,
+        tipus: "video",
+        leiras:
+        `Szeretnétek megismerni a Hatás/Ellenhatás törvényét? A könyvemben ez és sok más érdekes fizikai törvényről olvashatsz! Már kapható!`
     },
-    {
-        nev: "hello7"
-    },
-    {
-        nev: "hello8"
-    },
+
+    
 ]
