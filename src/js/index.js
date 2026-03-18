e389b33476c8aafade6b18a3a7395450b0ff72b8
adrianhelllo <hetessyadrian@gmail.com>
2026-03-18 19:09:15 +0100

    fix images not appearing on index.html

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,14 @@ for (let i = 0; i < postok.length; i++) {
   container.innerHTML += `
     <article class="post" id="${post.feltolto}-${post.sorszam}">
       <div class="post-author">
-        <img class="author-pfp" src="/assets/site/img/${post.feltolto}_pfp.png" alt="Profile picture" height="35" width="35">
+        <img class="author-pfp" src="assets/site/img/${post.feltolto}_pfp.png" alt="Profile picture" height="35" width="35">
         <a href="src/pages/profile.html?user=${post.feltolto}" class="author-name">${post.feltolto}</a>
       </div>
       <figure class="post-content">
         ${
           post.tipus === "kep"
-            ? `<img src="/assets/site/img/${post.feltolto}_post${post.sorszam}.png" alt="">`
-            : `<img src="/assets/site/img/${post.feltolto}_post${post.sorszam}.gif" alt="">`
+            ? `<img src="assets/site/img/${post.feltolto}_post${post.sorszam}.png" alt="">`
+            : `<img src="assets/site/img/${post.feltolto}_post${post.sorszam}.gif" alt="">`
         }
         <figcaption>${post.leiras}</figcaption>
       </figure>
