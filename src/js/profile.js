ec8df705ef64be9d4afacc446d12d6b2ac521396
opel-astra-g <kis.csaba.d@gmail.com>
2026-03-19 19:02:24 +0100

    profile content loading, javascript fixes

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,4 +1,4 @@
-import { profilok } from "./data.js";
+import { profilok, postok } from "./data.js";
 
 let params = new URLSearchParams(window.location.search);
 let username = params.get('user')
@@ -14,8 +14,23 @@ console.log(obj)
 document.getElementsByClassName('name')[0].innerHTML = obj.becenev
 document.getElementsByClassName('username')[0].innerHTML = obj.nev
 document.getElementsByClassName('introduction')[0].innerHTML = obj.leiras
+document.getElementsByClassName('introduction')[1].innerHTML = obj.leiras
 //document.getElementById('profile_picture').src = obj.profilkep
 
 
 document.title = `@${username} - lightenedgram`
 document.getElementById('profile_picture').src = `../../assets/site/img/${username}_pfp.png` // a weboldal nem fogja betölteni az esetleges placeholdert sajnos
+
+for (let i = 0; i < postok.length; i++) {
+    if (postok[i].feltolto == username) {
+        const post = postok[i]
+
+        console.log(`<img src="../../assets/site/img/${username}_post${post.sorszam}.png" alt=""></img>`)
+
+        document.getElementsByClassName("profile_posts")[0].innerHTML += `
+        ${post.tipus === "kep"
+            ? `<img src="../../assets/site/img/${username}_post${post.sorszam}.png" alt="">`
+            : `<img src="../../assets/site/img/${username}_post${post.sorszam}.gif" alt="">`
+        }`
+    }
+}
