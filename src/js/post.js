6188823e078952e68c347fa089547d9c1b883737
adrianhelllo <hetessyadrian@gmail.com>
2026-03-30 20:34:18 +0200

    attach img src attribute to js

--- src/js/post.js
+++ src/js/post.js
@@ -1,6 +1,7 @@
 import { kerdesek } from "./data.js";
 
 function showPost(uploader, number) {
+    // A keresett poszt megtalálása
     let post;
     for (let k of kerdesek) {
         if (k.feltolto === uploader && k.sorszam === number) {
@@ -9,6 +10,5 @@ function showPost(uploader, number) {
     };
 
     let main = document.getElementsByClassName("page-inner")[0];
-    main.innerHTML += `
-        
-    `
+    document.getElementById("post-float-img").src = `../../assets/site/img/${post.feltolto}_post${post.sorszam}`;
+}
